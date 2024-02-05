9699e1b954409256cde765ee5d9e5fe2bf9a28e7
saurabh chauhan <sourabh1202@gmail.com>
2024-02-05 11:37:53 +0200

    added swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import testRouter from "./routers/test-router";
-const swaggerUi = require('swagger-ui-express');
+import swaggerUi,{SwaggerOptions} from "swagger-ui-express";
 
 const server = express();
 
 server.use("/api/v1/test-route",testRouter);
+
+const swaggerOptions: SwaggerOptions = {
+    swaggerOptions: {
+      url: "/swagger.json",
+    },
+  };
+  
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-call
+  server.use("/docs", swaggerUi.serve, swaggerUi.setup(undefined, swaggerOptions));
 
 export default server;
