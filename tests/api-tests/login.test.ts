a41c9190aed3eb968f68e78a1459977b37997196
saurabh chauhan <sourabh1202@gmail.com>
2024-04-25 23:21:51 +0300

    uncommented login tests

--- tests/api-tests/login.test.ts
+++ tests/api-tests/login.test.ts
@@ -9,26 +9,26 @@ describe("Login API", () => {
       .send({ username: "", password: "" });
     expect(response.status).toEqual(403);
   });
-  // it("should authenticate the user with correct credentials", async () => {
-  //   const response = await app
-  //     .post("/api/v1/user/login")
-  //     .send({ username: "testuser", password: "testpassword" });
-  //   expect(response.status).toEqual(200);
-  //   expect(response.body.authtoken).toBeDefined();
-  // });
-  // it("should not authenticate the user with invalid credentials", async () => {
-  //   const response = await app
-  //     .post("/api/v1/user/login")
-  //     .send({ username: "wrongusername", password: "wrongpassword" });
-  //   expect(response.status).toEqual(404);
-  //   expect(response.body.message).toEqual("User not found");
-  // });
-  // it("should not authenticate the user with invalid password", async () => {
-  //   const response = await app
-  //     .post("/api/v1/user/login")
-  //     .send({ username: "testuser", password: "wrongpassword" });
-  //   expect(response.status).toEqual(401);
-  // });
+  it("should authenticate the user with correct credentials", async () => {
+    const response = await app
+      .post("/api/v1/user/login")
+      .send({ username: "testuser", password: "testpassword" });
+    expect(response.status).toEqual(200);
+    expect(response.body.authToken).toBeDefined();
+  });
+  it("should not authenticate the user with invalid credentials", async () => {
+    const response = await app
+      .post("/api/v1/user/login")
+      .send({ username: "wrongusername", password: "wrongpassword" });
+    expect(response.status).toEqual(404);
+    expect(response.body.message).toEqual("User not found");
+  });
+  it("should not authenticate the user with invalid password", async () => {
+    const response = await app
+      .post("/api/v1/user/login")
+      .send({ username: "testuser", password: "wrongpassword" });
+    expect(response.status).toEqual(401);
+  });
   it("should not authenticate with invalid form of data", async () => {
     const response = await app
       .post("/api/v1/user/login")
